c0b0e42411d5d1e87023f544474b5097b11ee3db
Phil Reinhold <pcreinhold@gmail.com>
2015-01-28 15:26:30 -0500

    use requests/xpath instead of rss

--- arxiv_mailer.py
+++ arxiv_mailer.py
@@ -1,12 +1,32 @@
 import traceback
-import feedparser
-from HTMLParser import HTMLParser
-# noinspection PyUnresolvedReferences
 from email.MIMEText import MIMEText
 from smtplib import SMTP
 import re
 import logging
 import sys
+
+import time
+import requests
+from lxml import html
+
+
+subject = 'quant-ph'
+sender = 'lazarusnotifier@gmail.com'
+passwd = 'electrons'
+receiver = 'pcreinhold@gmail.com'
+alerted_filename = 'arxiv_previously_alerted.txt'
+
+search_authors = [
+    "Robert Schoelkopf","Michel Devoret", "Liang Jiang", "Steve Girvin",
+    "Gerhard Kirchmair", "Hanhee Paik", "David Schuster",
+    "Leonardo DiCarlo", "Andrew Houck", "Johannes Majer",
+    "Adreas Walraff", "Irfan Siddiqi", "Konrad Lehnert",
+    "Vladimir Manucharyan", "Flavius Schackert",
+    "Blake Johnson", "Jerry Chow", "Jay Gambetta", "John Teufel",
+    "Matthew Reed", "John Martinis", "Andrew Cleland",
+    "Alexandre Blais", "David Cory", "Raymand Laflamme",
+    "Adrian Lupascu", "Leo Kouwenhoven", "Scott Aaronson",
+    "Joseph Emerson", "David Lyons", "Rajamani Vijay", "Bob Coecke"]
 
 
 def log_uncaught_exceptions(ex_cls, ex, tb):
@@ -20,55 +40,23 @@ logging.basicConfig(
     format="%(levelname)s : %(asctime)s : %(message)s"
 )
 logging.info("started arxiv search")
-
-subject = 'quant-ph'
-arxiv = 'http://arxiv.org/rss/' + subject
-rss = feedparser.parse(arxiv)
-logging.debug(','.join(i['title'] for i in rss['items']))
-
-search_authors = [
-    "Robert Schoelkopf","Michel Devoret", "Liang Jiang", "Steve Girvin",
-    "Gerhard Kirchmair", "Hanhee Paik", "David Schuster",
-    "Leonardo DiCarlo", "Andrew Houck", "Johannes Majer",
-    "Adreas Walraff", "Irfan Siddiqi", "Konrad Lehnert",
-    "Vladimir Manucharyan", "Flavius Schackert",
-    "Blake Johnson", "Jerry Chow", "Jay Gambetta", "John Teufel",
-    "Matthew Reed", "John Martinis", "Andrew Cleland",
-    "Alexandre Blais", "David Cory", "Raymand Laflamme",
-    "Adrian Lupascu", "Leo Kouwenhoven", "Scott Aaronson",
-    "Joseph Emerson", "David Lyons"]
-
-sender = 'lazarusnotifier@gmail.com'
-passwd = ''
-receiver = 'pcreinhold@gmail.com'
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 try:
-    previously_alerted = open('arxiv_previously_alerted.txt').readlines()
-    logging.info("arxiv_previously_alerted.txt found")
-    logging.debug(",".join(previously_alerted))
+    previously_alerted = open(alerted_filename, 'r').read().split('\n')
+    previously_alerted = map(str.strip, previously_alerted)
+    previously_alerted = filter(lambda x: x, previously_alerted)
+    logging.info("%s found", alerted_filename)
+    logging.info(",".join(previously_alerted))
 except IOError:
     previously_alerted = []
-    logging.warn("arxiv_previously_alerted.txt not found")
-
-def strip_tags(html):
-    s = HTMLStripper()
-    s.feed(html)
-    return s.get_data()
-
-class HTMLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
+    logging.warn("%s not found", alerted_filename)
 
 def match_name(name, text):
     first, last = name.split(' ')
     return first[0] == text[0] and last == text.split(' ')[-1]
 
-def email_html(src, passwd, dest, text, subj):
+def send_email_html(src, passwd, dest, text, subj):
     t = MIMEText(text, 'html')
     t['Subject'] = subj
     t['To'] = dest
@@ -78,28 +66,50 @@ def email_html(src, passwd, dest, text, subj):
     s.sendmail(src, dest, t.as_string())
     logging.info("Sent mail to " + dest)
 
-to_alert = []
-for item in rss['items']:
-    if item['id'] in previously_alerted:
+def get_tree(url):
+    page = requests.get(url)
+    return html.fromstring(page.text.encode('utf-8'))
+
+email_body = ""
+new_alerts = []
+tree = get_tree('http://arxiv.org/list/quant-ph/pastweek?show=200')
+tree.make_links_absolute('http://arxiv.org')
+atrees = tree.xpath('//div[@class="list-authors"]')
+agroups = [atree.xpath('a/text()') for atree in atrees]
+ids = tree.xpath('//a[@title="Abstract"]/text()')
+urls = tree.xpath('//a[@title="Abstract"]/@href')
+links = tree.xpath('//span[@class="list-identifier"]')
+assert atrees
+assert agroups
+assert ids
+assert urls
+assert links
+
+for authors, ident, links, url in zip(agroups, ids, links, urls):
+    if ident in previously_alerted:
+        logging.info("%s previously alerted, skipping", ident)
         continue
-    authors = map(strip_tags, item['author'].split(','))
-    logging.info('authors: %s' % authors)
     if any(any(match_name(sa, a) for a in authors) for sa in search_authors):
-        to_alert.append(item)
+        new_alerts.append(ident)
+        item_tree = get_tree(url)
+        blocks = item_tree.xpath('//div[@class="leftcolumn"]')[0].getchildren()[1:5]
+        blocks.insert(1, links)
+        email_body += "\n".join(map(html.tostring, blocks))
+        time.sleep(1) # be nice...
 
-if to_alert:
-    blocks = []
-    for item in to_alert:
-        title_str = re.split(r' \(arXiv.*\)$', item['title'])[0]
-        title_html = '<h3><a href="%s">%s</a></h3>' % (item['link'], title_str)
-        blocks.append('\n</br>\n'.join([title_html, item['author'], item['summary']]))
+css = "<style>%s</style>" % open("arXiv.css").read()
+email_html = "<html><head>%s</head><body>%s</body></html>" % (css, email_body)
 
-    alert_html = "\n</br>\n".join(blocks)
+with open('arxiv_email.html', 'w') as f:
+    f.write(email_html)
 
-    email_html(sender, passwd, receiver, alert_html, 'arXiv update')
+with open(alerted_filename, 'a') as f:
+    f.write('\n'.join(new_alerts) + '\n')
 
-    open('_previously_alerted.txt', 'a+').writelines([i['id'] for i in to_alert])
+if new_alerts:
+    logging.info('%d new items, emailing', len(new_alerts))
+    send_email_html(sender, passwd, receiver, email_html, 'arXiv update')
 else:
-    logging.info("Nothing to alert, exiting without emailing")
+    logging.info('No new items')
 
 logging.info("completed arxiv search")
